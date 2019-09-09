06ae8bc8320fa3617add392b9258a6876b1a2658
Miles Lacek <mileslacek@gmail.com>
2019-09-09 15:03:58 -0500

    Testing routes.

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -3,6 +3,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 class LoginPage extends Component {
   state = {
     username: '',
